f09e4a3acac6edc3ad91f9ce842d998932f8ecb5
Bosskboy <kokazono380@gmail.com>
2025-01-29 17:24:35 +0700

    This is lession one.

---
+++ Variable/main.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+#include <iomanip>
+int main() {
+    system("cls");
+    system("color 0A");
+    // cout<<"===================| Input Number |================="<<endl;
+
+    // int id ;
+    // string name;
+    // float salary,hour,wage;
+
+    // cout<<"Pleas Input ID: ";cin>>id;
+    // cout<<"Pleas Input Name: ";cin>>name;
+    // cout<<"Pleas Input Salary: ";cin>>salary;
+    // cout<<"Pleas Input Hour: ";cin>>hour;
+    // cout<<"Pleas Input Wide: ";cin>>wage;
+
+    // cout<<"================================================="<<endl;
+    
+    // cout<<" ID: "<<id<<endl;
+    // cout<<" Name: "<<name<<endl;
+    // cout<<" Salary: "<<salary<<endl;
+    // cout<<" Hour: "<<hour<<endl;
+    // cout<<" Wide: "<<wage<<"$ / 1h "<<endl;
+    //  float total  = salary + hour * wage;
+    // cout<<"Total wide "<<total<<" $";
+
+    // This AVG of student
+
+    string name;
+    float math,physic,chemistry,biology,history,geography,avg,total;
+    cout<<"Pleas Input Name: ";cin>>name;
+    cout<<"Pleas Input Math: ";cin>>math;
+    cout<<"Pleas Input Physic: ";cin>>physic;
+    cout<<"Pleas Input Chemistry: ";cin>>chemistry;
+    cout<<"Pleas Input Biology: ";cin>>biology;
+    cout<<"Pleas Input History: ";cin>>history;
+    cout<<"Pleas Input Geography: ";cin>>geography;
+    avg = (math + physic + chemistry + biology + history + geography) / 6;
+    total = math + physic + chemistry + biology + history + geography;
+    cout<<"================================================="<<endl;
+    cout<<" Name: "<<name<<endl;
+    cout<<" Math: "<<math<<endl;
+    cout<<" Physic: "<<physic<<endl;
+    cout<<" Chemistry: "<<chemistry<<endl;
+    cout<<" Biology: "<<biology<<endl;
+    cout<<" History: "<<history<<endl;
+    cout<<" Geography: "<<geography<<endl;
+    cout<<" AVG: "<<avg<<endl;
+    cout<<" Total: "<<total<< "Score"<<endl;
+
+    // tenery opereter
+    // string result = (x > y) ? "x is greater than y " : "y is greater than x";
+    // cout<<result<<endl;
+    // if(x <= 0){
+    //     cout<<"Invalid option!"<<endl;
+    // }else{
+    //     cout<<"You age is "<<x<<endl;
+    // }
+
+    return 0;
+}
