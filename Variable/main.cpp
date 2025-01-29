924fa5b4fdc92a01065f93318a915856f128565f
Bosskboy <kokazono380@gmail.com>
2025-01-29 18:14:28 +0700

    Update

--- Variable/main.cpp
+++ Variable/main.cpp
@@ -28,28 +28,45 @@ int main() {
 
     // This AVG of student
 
-    string name;
-    float math,physic,chemistry,biology,history,geography,avg,total;
-    cout<<"Pleas Input Name: ";cin>>name;
-    cout<<"Pleas Input Math: ";cin>>math;
-    cout<<"Pleas Input Physic: ";cin>>physic;
-    cout<<"Pleas Input Chemistry: ";cin>>chemistry;
-    cout<<"Pleas Input Biology: ";cin>>biology;
-    cout<<"Pleas Input History: ";cin>>history;
-    cout<<"Pleas Input Geography: ";cin>>geography;
-    avg = (math + physic + chemistry + biology + history + geography) / 6;
-    total = math + physic + chemistry + biology + history + geography;
-    cout<<"================================================="<<endl;
-    cout<<" Name: "<<name<<endl;
-    cout<<" Math: "<<math<<endl;
-    cout<<" Physic: "<<physic<<endl;
-    cout<<" Chemistry: "<<chemistry<<endl;
-    cout<<" Biology: "<<biology<<endl;
-    cout<<" History: "<<history<<endl;
-    cout<<" Geography: "<<geography<<endl;
-    cout<<" AVG: "<<avg<<endl;
-    cout<<" Total: "<<total<< "Score"<<endl;
+//     string name;
+//     float math,physic,chemistry,biology,history,geography,avg,total;
+//     cout<<"Pleas Input Name: ";cin>>name;
+//     cout<<"Pleas Input Math: ";cin>>math;
+//     cout<<"Pleas Input Physic: ";cin>>physic;
+//     cout<<"Pleas Input Chemistry: ";cin>>chemistry;
+//     cout<<"Pleas Input Biology: ";cin>>biology;
+//     cout<<"Pleas Input History: ";cin>>history;
+//     cout<<"Pleas Input Geography: ";cin>>geography;
+//     avg = (math + physic + chemistry + biology + history + geography) / 6;
+//     total = math + physic + chemistry + biology + history + geography;
+   
+    
+    
+//     cout<<"================================================="<<endl;
+//     cout<<" Name: "<<name<<endl;
+//     cout<<" Math: "<<math<<endl;
+//     cout<<" Physic: "<<physic<<endl;
+//     cout<<" Chemistry: "<<chemistry<<endl;
+//     cout<<" Biology: "<<biology<<endl;
+//     cout<<" History: "<<history<<endl;
+//     cout<<" Geography: "<<geography<<endl;
+//     cout<<" AVG: "<<avg<<endl;
+//   //  cout<<" Total: "<<total<< " Score"<<endl;
+//  if (total <= 300){
+//         cout<<"You are fail!"<<endl;
+//         cout<<"Your toal score is "<<total<<endl;
+//     }else{
+//         cout<<"You are pass!"<<endl;
+//         cout<<"Your toal score is "<<total<<endl;
+//     }
 
+
+    unsigned int x; // This is unsigned int
+    cout<<"Pleas Input Age: ";cin>>x;
+    char result = 96; // This is ASCII code
+    cout<<result<<endl;	
+    result = '/u00A9'; // This is unicode
+    cout<<result<<endl;
     // tenery opereter
     // string result = (x > y) ? "x is greater than y " : "y is greater than x";
     // cout<<result<<endl;
